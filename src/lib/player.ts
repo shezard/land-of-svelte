63ea5c0b56eab83cd8d1906277c271892073f5f0
unknown <dam.fayol@gmail.com>
2023-05-27 18:17:54 +0200

    Add zqsd + ad movment

---
+++ src/lib/player.ts
@@ -0,0 +1,41 @@
+import { writable } from "svelte/store";
+
+type PlayerPosition = [number, number, number, number];
+
+const createPosition = () => {
+    const { subscribe, update } = writable([0, 0, 10, 0] as PlayerPosition);
+
+    return {
+        subscribe,
+        moveForward: () => update(([x, y, z, theta]) => {
+            x += Math.sin(theta);
+            z -= Math.cos(theta);
+            return [x, y, z, theta];
+        }),
+        moveBackward: () => update(([x, y, z, theta]) => {
+            x -= Math.sin(theta);
+            z += Math.cos(theta);
+            return [x, y, z, theta];
+        }),
+        moveLeft: () => update(([x, y, z, theta]) => {
+            x -= Math.cos(theta);
+            z -= Math.sin(theta);
+            return [x, y, z, theta];
+        }),
+        moveRight: () => update(([x, y, z, theta]) => {
+            x += Math.cos(theta);
+            z += Math.sin(theta);
+            return [x, y, z, theta];
+        }),
+        rotateLeft: () => update(([x, y, z, theta]) => {
+            theta -= Math.PI / 2
+            return [x, y, z, theta];
+        }),
+        rotateRight: () => update(([x, y, z, theta]) => {
+            theta += Math.PI / 2
+            return [x, y, z, theta];
+        }),
+    }
+}
+
+export const position = createPosition();
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script>
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+
+    import { position } from '../lib/player';
+
+    const keyPress = (e) => {
+        if(e.key === 'z') {
+            position.moveForward();
+        }
+        if(e.key === 'q') {
+            position.moveLeft();
+        }
+        if(e.key === 's') {
+            position.moveBackward();
+        }
+        if(e.key === 'd') {
+            position.moveRight();
+        }
+        if(e.key === 'a') {
+            position.rotateLeft();
+        }
+        if(e.key === 'e') {
+            position.rotateRight();
+        }
+    }
+
 </script>
+
+<svelte:window 
+    on:keypress={keyPress}
+/>
 
 <SC.Canvas
     antialias
@@ -25,6 +53,9 @@
 
     <SC.AmbientLight intensity={0.5} />
     <SC.DirectionalLight intensity={0.5} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
-	<SC.PerspectiveCamera position={[0, 0, 10]} />
+	<SC.PerspectiveCamera 
+        position={[$position[0], $position[1], $position[2]]}
+        target={[$position[0] + Math.sin($position[3]), 0, $position[2] - Math.cos($position[3])]}
+    />
     <SC.OrbitControls maxPolarAngle={Math.PI * 0.51} />
 </SC.Canvas>
