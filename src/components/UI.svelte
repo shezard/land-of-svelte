a93686fc9fd420aec3364b80462746681a4a205a
unknown <dam.fayol@gmail.com>
2023-06-08 20:39:03 +0200

    Add animateOnce, to fix issue with door click retriggering opening

--- src/components/UI.svelte
+++ src/components/UI.svelte
@@ -12,16 +12,16 @@
 
 <style>
 	.border-dark {
-        padding-bottom: 4px;
-        width: calc(100vw - 8px);
-        box-sizing: content-box;
+		padding-bottom: 4px;
+		width: calc(100vw - 8px);
+		box-sizing: content-box;
 		border-style: inset;
 		border-color: #542b29;
 	}
 
 	.border-light {
-        padding-left: 4px;
-        box-sizing: border-box;
+		padding-left: 4px;
+		box-sizing: border-box;
 		border-style: inset;
 		border-color: #9c6b65;
 		background: #9a9a8e;
--- src/lib/scripts.ts
+++ src/lib/scripts.ts
@@ -1,14 +1,18 @@
 import type { Writable } from 'svelte/store';
 import type { Store } from '..';
 
-const animate = (cb: (t: number) => void, duration: number) => {
+const animate = (
+	cb: (store: Writable<Store>, t: number) => void,
+	store: Writable<Store>,
+	duration: number
+) => {
 	let t = 0;
 	const start = Date.now();
 
 	const step = () => {
 		requestAnimationFrame(() => {
-			t = (Date.now() - start) / 1e3;
-			cb(Math.min(t, 1));
+			t = (Date.now() - start) / 1e3 / duration;
+			cb(store, Math.min(t, 1));
 			if (t < duration) {
 				step();
 			}
@@ -18,26 +22,51 @@ const animate = (cb: (t: number) => void, duration: number) => {
 	step();
 };
 
+const animateOnce = (cb: (store: Writable<Store>, t: number) => void, duration: number) => {
+	let called = false;
+
+	return (store: Writable<Store>) => {
+		if (called) {
+			return;
+		}
+
+		called = true;
+
+		let t = 0;
+		const start = Date.now();
+
+		const step = () => {
+			requestAnimationFrame(() => {
+				t = (Date.now() - start) / 1e3 / duration;
+				cb(store, Math.min(t, 1));
+				if (t < duration) {
+					step();
+				}
+			});
+		};
+
+		step();
+	};
+};
+
 export const scripts = [
 	[
 		{
 			itemId: 1,
 			action: 'click',
-			doAction: (store: Writable<Store>) => {
-				animate((t) => {
-					store.update((store: Store) => {
-						const item = store.levels[0].getItem(0);
-						if (item) {
-							item.z = t * 1.01;
-							if (item.z > 1) {
-								item.collision = false;
-							}
-							store.levels[0].replaceItem(item);
+			doAction: animateOnce((store: Writable<Store>, t: number) => {
+				store.update((store: Store) => {
+					const item = store.levels[0].getItem(0);
+					if (item) {
+						item.z = t * 1.01;
+						if (item.z > 1) {
+							item.collision = false;
 						}
-						return store;
-					});
-				}, 1);
-			}
+						store.levels[0].replaceItem(item);
+					}
+					return store;
+				});
+			}, 1)
 		},
 		{
 			action: 'walk',
