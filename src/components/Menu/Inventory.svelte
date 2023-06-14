36aa1cce86f923bc07d873d52515bf1564bd21df
unknown <dam.fayol@gmail.com>
2023-06-14 07:27:17 +0200

    Add basic tooltip display

--- src/components/Menu/Inventory.svelte
+++ src/components/Menu/Inventory.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { store } from '$stores/store';
-	import type { Item } from '../..';
+	import type { Item, Stats } from '../..';
 
 	const toBag = (item: Item) => () => {
 		store.update((store) => {
 			store.player.inventory = store.player.unequip(item);
 			return store;
 		});
+		tooltip = false;
 	};
 
 	const toInventory = (item: Item, itemIndex: number) => () => {
@@ -14,6 +15,33 @@
 			store.player.inventory = store.player.equip(item, itemIndex);
 			return store;
 		});
+		tooltip = false;
+	};
+
+	let tooltip = false;
+	let tooltipX = 0;
+	let tooltipY = 0;
+	let tooltipStats: Stats = {
+		hp: 0,
+		maxHp: 0,
+		ac: 0,
+		hit: 0,
+		pDefense: 0,
+		pAttack: 0
+	};
+
+	const showTooltip = (item: Item) => () => {
+		tooltip = true;
+		tooltipStats = item.stats;
+	};
+
+	const hideTooltip = () => {
+		tooltip = false;
+	};
+
+	const moveTooltip = (e: Event) => {
+		tooltipX = e.layerX + 15;
+		tooltipY = e.layerY + 15;
 	};
 
 	$: mainHand = $store.player.inventory.mainHand;
@@ -27,16 +55,16 @@
 	<div class="text-3xl text-white">Character</div>
 
 	<div class="grid grid-cols-3">
-        <div class="flex flex-col text-white">
+		<div class="flex flex-col text-white">
 			<div class="text-2xl">
 				Stats:
-                <div class="pl-10">
-                    AC : {stats.ac} <br />
-                    Hit : {stats.hit} <br />
-                    HP : {stats.hp} <br />
-                    Attack : {stats.pAttack} <br />
-                    Defense : {stats.pDefense} <br />
-                </div>
+				<div class="pl-10">
+					AC : {stats.ac} <br />
+					Hit : {stats.hit} <br />
+					HP : {stats.hp} <br />
+					Attack : {stats.pAttack} <br />
+					Defense : {stats.pDefense} <br />
+				</div>
 			</div>
 		</div>
 
@@ -48,6 +76,9 @@
 						src="textures/{mainHand.texture}.png"
 						alt={mainHand.name}
 						on:click={toBag(mainHand)}
+						on:mouseenter={showTooltip(mainHand)}
+						on:mouseleave={hideTooltip}
+						on:mousemove={moveTooltip}
 						on:keypress={() => {
 							//no-op
 						}}
@@ -62,6 +93,9 @@
 						src="textures/{offHand.texture}.png"
 						alt={offHand.name}
 						on:click={toBag(offHand)}
+						on:mouseenter={showTooltip(offHand)}
+						on:mouseleave={hideTooltip}
+						on:mousemove={moveTooltip}
 						on:keypress={() => {
 							//no-op
 						}}
@@ -76,6 +110,9 @@
 						src="textures/{armor.texture}.png"
 						alt={armor.name}
 						on:click={toBag(armor)}
+						on:mouseenter={showTooltip(armor)}
+						on:mouseleave={hideTooltip}
+						on:mousemove={moveTooltip}
 						on:keypress={() => {
 							//no-op
 						}}
@@ -91,6 +128,9 @@
 					src="textures/{item.texture}.png"
 					alt={item.name}
 					on:click={toInventory(item, index)}
+					on:mouseenter={showTooltip(item)}
+					on:mouseleave={hideTooltip}
+					on:mousemove={moveTooltip}
 					on:keypress={() => {
 						//no-op
 					}}
@@ -98,12 +138,28 @@
 			{/each}
 		</div>
 	</div>
+
+	<div
+		class="info text-2xl text-white absolute hidden"
+		class:tooltip
+		style="top:{tooltipY}px;left:{tooltipX}px;"
+	>
+		AC : {tooltipStats.ac} <br />
+		Hit : {tooltipStats.hit} <br />
+		HP : {tooltipStats.hp} <br />
+		Attack : {tooltipStats.pAttack} <br />
+		Defense : {tooltipStats.pDefense} <br />
+	</div>
 </div>
 
 <style>
 	img {
 		image-rendering: pixelated;
 		width: 24px;
-		@apply inline;
+		@apply inline cursor-pointer;
+	}
+
+	.tooltip {
+		display: block;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,9 @@
 <slot />
 
 <style global>
-	:global(body) {
+	:global(html, body) {
+		width: 100%;
+		height: 100%;
 		overflow: hidden;
 	}
 
